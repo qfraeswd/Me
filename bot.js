5e8ac576d85b0147cf4e5e8e806bd4d126bb1777
qfraeswd <47043267+qfraeswd@users.noreply.github.com>
2019-05-19 23:13:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3644,4 +3644,14 @@ client.on("message", (message) => {
                         }
             });
 
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const xkiller = member.guild.channels.find("name", "۞-invitations");
+     xkiller.send(`[@${member.user.id}] تمت الدعوه من [@${inviter.id}]`);
+  });
+});
+
 client.login(process.env.BOT_TOKEN);
